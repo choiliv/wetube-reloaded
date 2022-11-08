912dc28fd97c026f0bd93011c691b0d27c783618
choiliv <104757153+choiliv@users.noreply.github.com>
2022-11-08 18:10:20 +0900

    express와 middleware 사용

--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
 
 import express from "express";
 /* const express = require("express");와 같음*/
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-    return res.send("Nothing!!");
-}
 
-const handleLogin = (req, res) => {
-    return res.send("Login here!");
-}
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const home = (req, res) => {
+    return res.send("Hi");
+};
+
+const login = (req, res) => {
+    return res.send("Login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
 console.log(`server listening on port http://localhost:${PORT} 🚀`);
