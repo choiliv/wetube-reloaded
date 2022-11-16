28aaa0f1c4c02710d60e89ba411a08ce74f31a4e
choiliv <104757153+choiliv@users.noreply.github.com>
2022-11-16 22:00:28 +0900

    router와 controller 코드 정리

---
+++ src/routers/userRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { edit, remove} from "../controllers/userController";
+
+
+const userRouter = express.Router();
+
+
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
+
+export default userRouter;
--- src/server.js
+++ src/server.js
@@ -1,24 +1,21 @@
 
-import express from "express";
+import express, { Router } from "express";
 /* const express = require("express");와 같음*/
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
+
 const app = express();
 const logger = morgan("dev");
-
-
-const home = (req, res) => {
-    return res.send("Hi");
-};
-
-const login = (req, res) => {
-    return res.send("Login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
 console.log(`server listening on port http://localhost:${PORT} 🚀`);
