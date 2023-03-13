82d66e4ab3797260b31d20539c3bfbbd0c918a64
choiliv <104757153+choiliv@users.noreply.github.com>
2023-03-13 22:23:09 +0900

    Error: Can't login with github ID

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@
 import express, { Router } from "express";
 /* const express = require("express");와 같음*/
 import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -15,6 +18,15 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({extended:true}));
 app.use(logger);
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL}),
+}));
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
